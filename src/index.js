10c8e7aed4980f21c67267f58995a6af14faa845
Capi Etheriel <barraponto@gmail.com>
2017-03-17 22:06:26 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const stylelintPreset = require('neutrino-preset-stylelint');
+const stylelintConfiguration = { config: { extends: 'stylelint-config-standard' } };
+
+module.exports = (neutrino) => {
+  stylelintPreset(neutrino);
+  neutrino.config
+    .plugin('stylelint')
+    .inject((Plugin, options) => {
+      return new Plugin(Object.assign({}, options[0], stylelintConfiguration))
+    });
+};
