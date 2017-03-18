3f740575a2a40cd6aeee1649b48d4c314d67b3f6
Capi Etheriel <barraponto@gmail.com>
2017-03-18 01:04:43 -0300

    feat(configuration): Allow extending configuration from package.json

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const stylelintPreset = require('neutrino-preset-stylelint');
-const stylelintConfiguration = { config: { extends: require.resolve('stylelint-config-standard') } };
+const standardConfiguration = { extends: require.resolve('stylelint-config-standard') };
 
 module.exports = (neutrino) => {
   stylelintPreset(neutrino);
   neutrino.config
     .plugin('stylelint')
     .inject((Plugin, options) => {
-      return new Plugin(Object.assign({}, options[0], stylelintConfiguration))
+      const projectConfiguration = options[0].config ? options[0].config : {};
+
+      return new Plugin(Object.assign(
+        {},
+        options[0],
+        // let any configuration.extends overwrite our standard preset.
+        { config: Object.assign({}, standardConfiguration, projectConfiguration) })
+      );
     });
 };
