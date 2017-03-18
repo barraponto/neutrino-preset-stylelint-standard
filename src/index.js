003998f8242edcd40b060cca0919ea5d0bd24694
Capi Etheriel <barraponto@gmail.com>
2017-03-17 22:32:59 -0300

    fix resolving the stylelint config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const stylelintPreset = require('neutrino-preset-stylelint');
-const stylelintConfiguration = { config: { extends: 'stylelint-config-standard' } };
+const stylelintConfiguration = { config: { extends: require.resolve('stylelint-config-standard') } };
 
 module.exports = (neutrino) => {
   stylelintPreset(neutrino);
