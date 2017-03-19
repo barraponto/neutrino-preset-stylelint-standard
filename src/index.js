cc6e35122c520ebdb07aaf5b07535724e212d4de
Capi Etheriel <barraponto@gmail.com>
2017-03-19 10:06:24 -0300

    refactor(configuration): use deepmerge instead of nested Object.assign

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
+const merge = require('deepmerge');
 const stylelintPreset = require('neutrino-preset-stylelint');
-const standardConfiguration = { extends: require.resolve('stylelint-config-standard') };
 
 module.exports = (neutrino) => {
   stylelintPreset(neutrino);
   neutrino.config
     .plugin('stylelint')
-    .inject((Plugin, options) => {
-      const projectConfiguration = options[0].config ? options[0].config : {};
-
-      return new Plugin(Object.assign(
-        {},
-        options[0],
-        // let any configuration.extends overwrite our standard preset.
-        { config: Object.assign({}, standardConfiguration, projectConfiguration) })
-      );
-    });
+    .inject((Plugin, options) => new Plugin(merge({
+      config: {
+        extends: require.resolve('stylelint-config-standard'),
+      },
+    // let any configuration.extends overwrite our standard preset.
+    }, options)));
 };
